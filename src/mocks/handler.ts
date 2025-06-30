5d79390c630a2bb6be37734e7a9dce663c439fb8
maximedervaux <maximeder.pro@gmail.com>
2025-06-30 14:41:07 +0200

    feat: replace getPokemon.json with updated getPokemons.json for enhanced mock data

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,10 +1,24 @@
 import { http, HttpResponse } from 'msw'
-import getPokemon from './fixtures/getPokemon.json'
+import getPokemons from './fixtures/getPokemons.json'
 
 export const handlers = [
 
   // Mock GET /api/pokemons
-    http.get('http://localhost:3000/pokemon', () => {
-     return HttpResponse.json(getPokemon);
+    http.get('http://localhost:3000/pokemons', () => {
+     console.log('Mock : GET /pokemons');
+     console.log(getPokemons);
+     return HttpResponse.json(getPokemons);
    }),
+
+  // Mock GET /api/pokemons/:id
+  http.get('http://localhost:3000/pokemons/:id', (request) => {
+    const { id } = request.params;
+    console.log(`Mock : GET /pokemons/${id}`);
+    const pokemon = getPokemons.pokemons.find(p => p.id === Number(id));
+    if (!pokemon) {
+      return HttpResponse.json({ error: 'Pokemon not found' }, { status: 404 });
+    }
+    return HttpResponse.json(pokemon);
+  }),
+
 ];
